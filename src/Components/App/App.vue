bb5015c0510da9385f128f4ecb001f582eee9b13
Mathias Schrooten <mathiasschrooten@gmail.com>
2019-06-08 22:25:55 +0200

    update

--- src/Components/App/App.vue
+++ src/Components/App/App.vue
@@ -264,7 +264,7 @@ export default {
             this.loading = true;
 
             // Make the request to gateway with formatting enabled */
-            fetch('https://151583e1.ngrok.io/kor/getBotResponse', {method: 'POST', headers: {'content-type': 'application/json'}, body: JSON.stringify(request)})
+            fetch('https://scvirtualagent.chatwise.be/kor/getBotResponse', {method: 'POST', headers: {'content-type': 'application/json'}, body: JSON.stringify(request)})
             .then(response => {
                 console.log("response = ");
                 console.log(response);
