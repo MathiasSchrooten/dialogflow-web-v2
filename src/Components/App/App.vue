4dbb6731cd92fe82635135868845bf5915d30863
Mathias Schrooten <mathiasschrooten@gmail.com>
2019-06-22 22:02:14 +0200

    build

--- src/Components/App/App.vue
+++ src/Components/App/App.vue
@@ -288,7 +288,7 @@ export default {
             this.loading = true;
 
             // Make the request to gateway with formatting enabled */
-            fetch('https://imcc.chatbotkorea.net/kor/getBotResponse', {method: 'POST', headers: {'content-type': 'application/json'}, body: JSON.stringify(request)})
+            fetch('https://imcc.chatbotkorea.net/kor/getBotResponse', {method: 'POST', mode: 'cors', headers: {'content-type': 'application/json'}, body: JSON.stringify(request)})
             .then(response => {
                 console.log("response.json() = ");
                 return response.json();
